b3061578ea9a3c58510893b69c32bc0d512fef75
Haerangsa <87694108+Haerangsa@users.noreply.github.com>
2021-07-20 18:35:34 +0900

    python
    오늘은 파이썬을 공부했다.

---
+++ second.py
@@ -0,0 +1,18 @@
+yoonsung=10
+print(yoonsung)
+y=0.5
+print(y) 
+yoonsung = yoonsung / 3
+print(yoonsung==yoonsung)
+a = 'pig'
+b= 'dad'
+print(a+b)
+pig='asdf'
+print(a)
+family=['mother','father','gentleman']
+len(family)
+print(len(family))
+print(family[2])
+family[0]=1
+print(family[0])
+c=input('hello')
